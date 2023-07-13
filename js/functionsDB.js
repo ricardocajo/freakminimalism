bd33952c379119fb0c6e970d8121b77979d2f03f
Ricardo Cordeiro <ricas.cordeiro@gmail.com>
2023-07-13 10:40:07 +0100

    price list json url and test
    price list json url and test

--- js/functionsDB.js
+++ js/functionsDB.js
@@ -1,16 +1,32 @@
 let ABSOLUTE_PATH = "https://ricardocajo.github.io/freakminimalism"
 
+/* Fetches available sizes data and adds it to the Webpage */
 function initialize_sizes() {
-    let sizes_DOM = document.getElementById("sizes_list");
-    fetch(ABSOLUTE_PATH + "/db/sizes/sizes.json")
+  let sizes_DOM = document.getElementById("sizes_list");
+  fetch(ABSOLUTE_PATH + "/db/sizes/sizes.json")
+    .then(response => response.json())
+    .then(data => {
+      let sizes = Object.keys(data).filter(size => data[size]);
+      let sizesString = sizes.join(" | ");
+      sizes_DOM.innerText = sizesString;
+    })
+    .catch(error => {
+      console.error('Error:', error);
+    });
+}
+
+/* Fetches available prices data and adds it to the Webpage */
+function initialize_prices() {
+    let prices_DOM = document.getElementById("prices_list");
+    fetch(ABSOLUTE_PATH + "/db/prices/prices.json")
       .then(response => response.json())
       .then(data => {
-        let sizes = Object.keys(data).filter(size => data[size]);
-        let sizesString = sizes.join(" | ");
-        sizes_DOM.innerText = sizesString;
+        Object.keys(data).forEach(key => {
+            prices_DOM.appendChild(document.createElement("li")
+                .appendChild(document.createTextNode(key + "......." + data[key])));
+        });
       })
       .catch(error => {
         console.error('Error:', error);
       });
   }
-  
