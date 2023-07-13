e313749cfa82e87692145db1d1557a5da993293a
Ricardo Cordeiro <ricas.cordeiro@gmail.com>
2023-07-13 02:09:29 +0100

    get true keys and json fetch
    get true keys and json fetch

--- js/functionsDB.js
+++ js/functionsDB.js
@@ -1,11 +1,16 @@
+let ABSOLUTE_PATH = "https://ricardocajo.github.io/freakminimalism"
+
 function initialize_sizes() {
-  let sizes_DOM = document.getElementById("sizes_list");
-  fetch("https://ricardocajo.github.io/freakminimalism/db/sizes/sizes.json")
-    .then(response => response.json())
-    .then(data => {
-      sizes_DOM.innerText = JSON.stringify(data);
-    })
-    .catch(error => {
-      console.error('Error:', error);
-    });
-}
+    let sizes_DOM = document.getElementById("sizes_list");
+    fetch(ABSOLUTE_PATH + "/db/sizes/sizes.json")
+      .then(response => response.json())
+      .then(data => {
+        let sizes = Object.keys(data).filter(size => data[size]);
+        let sizesString = sizes.join(" | ");
+        sizes_DOM.innerText = sizesString;
+      })
+      .catch(error => {
+        console.error('Error:', error);
+      });
+  }
+  
