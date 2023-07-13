f3b69d026fcfc3d2b8220d27ca47abf6137fb669
Ricardo Cordeiro <ricas.cordeiro@gmail.com>
2023-07-13 17:44:48 +0100

    Prices function and html
    Prices function and html

--- js/functionsDB.js
+++ js/functionsDB.js
@@ -17,16 +17,24 @@ function initialize_sizes() {
 
 /* Fetches available prices data and adds it to the Webpage */
 function initialize_prices() {
-    let prices_DOM = document.getElementById("prices_list");
-    fetch(ABSOLUTE_PATH + "/db/prices/prices.json")
-      .then(response => response.json())
-      .then(data => {
-        Object.keys(data).forEach(key => {
-            prices_DOM.appendChild(document.createElement("li")
-                .appendChild(document.createTextNode(key + "......." + data[key])));
-        });
-      })
-      .catch(error => {
-        console.error('Error:', error);
+  let prices_DOM = document.getElementById("prices_list");
+  fetch(ABSOLUTE_PATH + "/db/prices/prices.json")
+    .then(response => response.json())
+    .then(data => {
+      const maxLength = Math.max(...Object.keys(data).map(key => key.length));
+      const monospaceFont = "Courier New, monospace"; // Change to your desired monospaced font
+      Object.keys(data).forEach(key => {
+        let listItem = document.createElement("li");
+        listItem.style.fontFamily = monospaceFont;
+        listItem.style.display = "flex";
+        listItem.style.justifyContent = "space-between";
+        listItem.innerHTML = `<span>${key}</span><span>${data[key]}</span>`;
+        prices_DOM.appendChild(listItem);
       });
-  }
+    })
+    .catch(error => {
+      console.error('Error:', error);
+    });
+}
+
+
