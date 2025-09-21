b3a2cd39e2ba0451d452cfa202586f9c37fba7ee
Ricardo Cordeiro <82663158+ricardocajo@users.noreply.github.com>
2025-09-21 14:32:22 +0100

    customize page

--- src/app/customize/page.tsx
+++ src/app/customize/page.tsx
@@ -501,6 +501,42 @@ export default function CustomizePage() {
                             placeholder="Descreva como deseja personalizar o seu item..."
                           />
                         </div>
+                        {/* Info boxes similar to Patches flow */}
+                        <div className="bg-yellow-50 border-l-4 border-yellow-400 p-4">
+                          <div className="flex">
+                            <div className="flex-shrink-0">
+                              <svg className="h-5 w-5 text-yellow-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+                                <path fillRule="evenodd" d="M8.257 3.099c.765-1.36 2.722-1.36 3.486 0l5.58 9.92c.75 1.334-.213 2.98-1.742 2.98H4.42c-1.53 0-2.493-1.646-1.743-2.98l5.58-9.92zM11 13a1 1 0 11-2 0 1 1 0 012 0zm-1-8a1 1 0 00-1 1v3a1 1 0 002 0V6a1 1 0 00-1-1z" clipRule="evenodd" />
+                              </svg>
+                            </div>
+                            <div className="ml-3">
+                              <p className="text-sm text-yellow-700">
+                                <span className="font-bold">Atenção:</span> Após enviar este pedido, você será direcionado para o WhatsApp. Por favor, envie uma foto ou imagem de referência do seu design como uma mensagem separada.
+                              </p>
+                            </div>
+                          </div>
+                        </div>
+
+                        <div className="bg-blue-50 p-4 rounded-lg border border-blue-200">
+                          <div className="flex items-start">
+                            <div className="flex-shrink-0">
+                              <svg className="h-5 w-5 text-blue-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+                                <path fillRule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h2a1 1 0 100-2v-3a1 1 0 00-1-1H9z" clipRule="evenodd" />
+                              </svg>
+                            </div>
+                            <div className="ml-3">
+                              <h3 className="text-sm font-medium text-blue-800">Como enviar seu design</h3>
+                              <div className="mt-1 text-sm text-blue-700">
+                                <ol className="list-decimal pl-4 space-y-1">
+                                  <li>Clique no botão abaixo para abrir o WhatsApp</li>
+                                  <li>O aplicativo irá abrir com uma mensagem pré-preenchida</li>
+                                  <li>Envie uma foto do seu design como resposta a esta mensagem</li>
+                                  <li>Certifique-se que a imagem está nítida e mostra todos os detalhes</li>
+                                </ol>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
                       </div>
                     </div>
                     
