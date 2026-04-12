ad47e0b0a5bac1d22afdba3554647361d8143f25
Ricardo Cordeiro <ricardo@opplane.com>
2026-04-12 21:01:37 +0100

    festa

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -25,7 +25,7 @@ const Home = async () => {
       {/* Google Maps embed section */}
       <section className="min-h-[60vh] sm:min-h-[50vh] flex flex-col items-center justify-center px-4 py-8 space-y-6">
         <div className="w-full max-w-2xl">
-          <iframe
+          {/*<iframe
             width="100%"
             height="400"
             style={{ border: 0, borderRadius: '0.5rem' }}
@@ -33,16 +33,29 @@ const Home = async () => {
             allowFullScreen
             referrerPolicy="no-referrer-when-downgrade"
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d897!2d-8.9758421!3d38.705919!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd19394e41250b15%3A0x7c512b28289c73b5!2sFREAK%20MINIMALISM!5e0!3m2!1spt-PT!2spt!4v1"
+          />*/}
+          <img 
+            src="/flyer.jpeg"
+            alt="Flyer festa" 
+            className="w-full h-auto rounded-lg shadow-lg"
           />
         </div>
         <div className="text-center">
-          <a
+          {/*<a
             href="https://www.google.com/maps/place/FREAK+MINIMALISM/@38.705919,-8.9758421,897m/data=!3m2!1e3!4b1!4m6!3m5!1s0xd19394e41250b15:0x7c512b28289c73b5!8m2!3d38.7059148!4d-8.9732618!16s%2Fg%2F11yqtvgslz?entry=ttu&g_ep=EgoyMDI2MDEwNi4wIKXMDSoKLDEwMDc5MjA3MUgBUAM%3D"
             target="_blank"
             rel="noopener noreferrer"
             className="text-lg hover:underline flex items-center justify-center gap-2"
           >
             📍 R. Alm. Cândido dos Reis 57, 2870-150 Montijo
+          </a>*/}
+          <a
+            href="https://api.whatsapp.com/send?phone=351960361839"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-lg hover:underline flex items-center justify-center gap-2"
+          >
+            📩 Adquirir Bilhetes 📩
           </a>
         </div>
       </section>
